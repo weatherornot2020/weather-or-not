dc6fa490b82bd7398716bd0f717007a1e8f3b46d
Andrew Jones <aej11@hotmail.com>
2020-03-28 16:39:25 -0400

    Added text to test the auto deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Weather or Not</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
