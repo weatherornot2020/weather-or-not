c26cfd5dd86302f7adab2f03280492fc40e3bcdf
Andrew Jones <aej11@hotmail.com>
2020-03-28 16:44:12 -0400

    Added ! to test the auto deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to Weather or Not</h1>
+        <h1>Welcome to Weather or Not!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
