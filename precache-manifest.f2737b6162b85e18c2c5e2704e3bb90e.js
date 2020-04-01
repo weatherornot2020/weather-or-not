8e288d4eab2fdcfd452b2b9f2f7eeccbf27230ab
aej11a <aej11a@users.noreply.github.com>
2020-04-01 21:05:25 +0000

    deploy: 1c8235955e22a513cee463c443b3434662dcfed5

---
+++ precache-manifest.f2737b6162b85e18c2c5e2704e3bb90e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2605d4bb792280adf98d9f779ca634a8",
+    "url": "/weather-or-not/index.html"
+  },
+  {
+    "revision": "339ba00509c74a91ee03",
+    "url": "/weather-or-not/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0705d2067192589fa7a2",
+    "url": "/weather-or-not/static/js/2.e75967c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-or-not/static/js/2.e75967c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339ba00509c74a91ee03",
+    "url": "/weather-or-not/static/js/main.34f5123b.chunk.js"
+  },
+  {
+    "revision": "0d580baf61addf7da2ed",
+    "url": "/weather-or-not/static/js/runtime-main.f2ba3696.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-or-not/static/media/logo.5d5d9eef.svg"
+  }
+]);
