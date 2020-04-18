24e080d8b49ba80dc87ebaea08ddca5501f672cc
aej11a <aej11a@users.noreply.github.com>
2020-04-18 04:52:51 +0000

    deploy: e1d833e4069e5dd5c588e1afb5d3bce9dd56c630

---
+++ precache-manifest.06794e263d1cb070f48a3fbeed97568c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330864b5e9c28ae6726d25ecc1f1e341",
+    "url": "/weather-or-not/index.html"
+  },
+  {
+    "revision": "d75a955a9ca76e3d0d65",
+    "url": "/weather-or-not/static/css/main.9ab4290d.chunk.css"
+  },
+  {
+    "revision": "ed3f9b430fe576193ffb",
+    "url": "/weather-or-not/static/js/2.f1a15b0c.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/weather-or-not/static/js/2.f1a15b0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75a955a9ca76e3d0d65",
+    "url": "/weather-or-not/static/js/main.38dfd3e4.chunk.js"
+  },
+  {
+    "revision": "0d580baf61addf7da2ed",
+    "url": "/weather-or-not/static/js/runtime-main.f2ba3696.js"
+  },
+  {
+    "revision": "0d7c0aaebd32b011a899e5770ec81bee",
+    "url": "/weather-or-not/static/media/Search.0d7c0aae.png"
+  },
+  {
+    "revision": "5d8840e6d4fb9814148dfedb3b018b29",
+    "url": "/weather-or-not/static/media/login.5d8840e6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-or-not/precache-manifest.857f12dd649860f5a3a7d77af5f8ff39.js"
+  "/weather-or-not/precache-manifest.06794e263d1cb070f48a3fbeed97568c.js"
 );
 
 self.addEventListener('message', (event) => {
